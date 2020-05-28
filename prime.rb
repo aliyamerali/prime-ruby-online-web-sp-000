0bea32497f3773bc08382b04508230904da19b3e
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-28 03:55:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -18,5 +18,15 @@ def prime?(num)
   end
 end
 
+def prime_refactored?(num)
+  if num <= 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
+
 # puts prime?(12)
 # puts Benchmark.measure {}
